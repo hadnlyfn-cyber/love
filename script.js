6a93e90515dc8d456dcd659e03601f43563d75b4
hadnlyfn-cyber <hadnlyfn@gmail.com>
2026-04-22 07:34:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,23 +9,3 @@ document.getElementById("startBtn").addEventListener("click", function() {
   // Показать экран игры
   document.getElementById("game").style.display = "block";
 });
-
-// Когда пользователь кликает на изображение (PNG)
-document.getElementById("clickableImage").addEventListener("click", function() {
-  // Скрыть экран игры
-  document.getElementById("game").style.display = "none";
-  
-  // Показать видео
-  document.getElementById("videoContainer").style.display = "block";
-  
-  // Запуск видео
-  const video = document.getElementById("loveVideo");
-  video.play();
-
-  // После завершения видео перенести на следующий этап
-  video.onended = function() {
-    alert("Ты прошла этот этап! Готовься к следующему!");
-    // Здесь можно добавить переход на следующий этап
-  };
-});
-console.log("JS загружен!");
