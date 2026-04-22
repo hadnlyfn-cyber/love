f84d132b1b1d97a1056b8cd36e957a35aa3f99b3
hadnlyfn-cyber <hadnlyfn@gmail.com>
2026-04-22 07:49:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Показать экран с картинкой и кнопкой
 document.getElementById("startScreen").style.display = "block";
 
-// При нажатии на кнопку "For My Love" начать игру
+// При нажатии на кнопку "For My Love" начать игру и запустить музыку
 document.getElementById("startBtn").addEventListener("click", function() {
   // Скрыть экран с картинкой и кнопкой
   document.getElementById("startScreen").style.display = "none";
   
   // Показать экран игры
   document.getElementById("game").style.display = "block";
+
+  // Запуск музыки
+  var music = document.getElementById("backgroundMusic");
+  music.play();
 });
